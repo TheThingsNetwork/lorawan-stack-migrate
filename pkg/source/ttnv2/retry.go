fc519ed0d1868eba70c3ab21212cb2c504afcc5a
Aggelos Kolaitis <aggelos.kolaitis@thethingsindustries.com>
2021-03-18 16:07:02 +0200

    all: Allow context cancellation during backoff

--- pkg/source/ttnv2/retry.go
+++ pkg/source/ttnv2/retry.go
@@ -32,42 +32,50 @@ type deviceManagerWithRetry struct {
 	backoff    time.Duration
 }
 
-func (d *deviceManagerWithRetry) shouldRetry(err error, try uint, fields log.Fielder) (bool, time.Duration) {
-	if err == nil {
+func (d *deviceManagerWithRetry) shouldRetry(ctx context.Context, err error, attempt uint) (bool, time.Duration) {
+	if err == nil || attempt >= d.maxRetries {
 		return false, 0
 	}
-	if err, ok := errors.From(err); ok && (errors.IsResourceExhausted(err) || errors.IsUnavailable(err)) && try < d.maxRetries {
-		penalty := d.backoff * time.Duration(try)
-		log.FromContext(d.ctx).WithError(err).WithField("try", try).WithFields(fields).Warnf("Non-fatal error, will retry after %v", penalty)
+	if err, ok := errors.From(err); ok && (errors.IsResourceExhausted(err) || errors.IsUnavailable(err)) {
+		penalty := d.backoff * time.Duration(attempt)
+		log.FromContext(d.ctx).WithError(err).WithField("attempt", attempt).Debugf("Non-fatal error, will retry after %v", penalty)
 		return true, penalty
 	}
 	return false, 0
 }
 
-func (d *deviceManagerWithRetry) get(devID string, try uint) (*ttnsdk.Device, error) {
+func (d *deviceManagerWithRetry) get(ctx context.Context, devID string, attempt uint) (*ttnsdk.Device, error) {
 	dev, err := d.DeviceManager.Get(devID)
-	if retry, penalty := d.shouldRetry(err, try, log.Fields("device_id", devID, "method", "Set")); retry {
-		time.Sleep(penalty)
-		return d.get(devID, try+1)
+	if retry, penalty := d.shouldRetry(ctx, err, attempt); retry {
+		select {
+		case <-time.After(penalty):
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		}
+		return d.get(ctx, devID, attempt+1)
 	}
 	return dev, err
 }
 
-func (d *deviceManagerWithRetry) set(dev *ttnsdk.Device, try uint) error {
+func (d *deviceManagerWithRetry) set(ctx context.Context, dev *ttnsdk.Device, attempt uint) error {
 	err := d.DeviceManager.Set(dev)
-	if retry, penalty := d.shouldRetry(err, try, log.Fields("dev_eui", dev.DevEUI, "device_id", dev.DevID, "method", "Set")); retry {
-		time.Sleep(penalty)
-		return d.set(dev, try+1)
+	if retry, penalty := d.shouldRetry(ctx, err, attempt); retry {
+		select {
+		case <-time.After(penalty):
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+		return d.set(ctx, dev, attempt+1)
 	}
 	return err
 }
 
 func (d *deviceManagerWithRetry) Get(devID string) (*ttnsdk.Device, error) {
-	return d.get(devID, 1)
+	return d.get(log.NewContextWithFields(d.ctx, log.Fields("device_id", devID, "method", "Get")), devID, 1)
 }
 
 func (d *deviceManagerWithRetry) Set(dev *ttnsdk.Device) error {
-	return d.set(dev, 1)
+	return d.set(log.NewContextWithFields(d.ctx, log.Fields("dev_eui", dev.DevEUI, "device_id", dev.DevID, "method", "Set")), dev, 1)
 }
 
 var _ ttnsdk.DeviceManager = &deviceManagerWithRetry{}
