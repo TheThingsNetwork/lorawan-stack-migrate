1a96dbec27ba4b835056fffae7ba261c8f909a50
happyRip <aleksander.borowski.01@gmail.com>
2022-09-09 15:35:35 +0200

    dev: Add basic config

---
+++ pkg/source/firefly/config.go
@@ -0,0 +1,14 @@
+package firefly
+
+import "github.com/spf13/pflag"
+
+type config struct{}
+
+func flagSet() *pflag.FlagSet {
+	flags := &pflag.FlagSet{}
+	return flags
+}
+
+func getConfig(flags *pflag.FlagSet) (*config, error) {
+	return &config{}, nil
+}
