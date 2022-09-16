3b758506327163076cc9d1ce29a6b71bd6a3f525
happyRip <aleksander.borowski.01@gmail.com>
2022-09-16 16:14:23 +0200

    dev: Work on `firefly` package

--- pkg/source/firefly/errors.go
+++ pkg/source/firefly/errors.go
@@ -3,8 +3,8 @@ package firefly
 import "go.thethings.network/lorawan-stack/v3/pkg/errors"
 
 var (
-	// errNoAppID  = errors.DefineInvalidArgument("no_app_id", "no app id")
-	errNoAPIURL  = errors.DefineInvalidArgument("no_api_url", "no api url")
 	errNoAPIKEY  = errors.DefineInvalidArgument("no_api_key", "no api key")
+	errNoAPIURL  = errors.DefineInvalidArgument("no_api_url", "no api url")
+	errNoAppID   = errors.DefineInvalidArgument("no_app_id", "no app id")
 	errNoJoinEUI = errors.DefineInvalidArgument("no_join_eui", "no join eui")
 )
--- pkg/source/firefly/source.go
+++ pkg/source/firefly/source.go
@@ -2,14 +2,16 @@ package firefly
 
 import (
 	"context"
-	"fmt"
 	"strings"
 
+	pbtypes "github.com/gogo/protobuf/types"
 	"github.com/spf13/pflag"
+	"go.thethings.network/lorawan-stack/v3/pkg/log"
 	"go.thethings.network/lorawan-stack/v3/pkg/ttnpb"
 	"go.thethings.network/lorawan-stack/v3/pkg/types"
 
 	"go.thethings.network/lorawan-stack-migrate/pkg/source"
+	"go.thethings.network/lorawan-stack-migrate/pkg/source/firefly/api"
 	"go.thethings.network/lorawan-stack-migrate/pkg/source/firefly/devices"
 )
 
@@ -44,15 +46,20 @@ func (s Source) ExportDevice(devEUI string) (*ttnpb.EndDevice, error) {
 		// Formatters:      &ttnpb.MessagePayloadFormatters{},
 		FrequencyPlanId: s.config.frequencyPlanID,
 		Ids: &ttnpb.EndDeviceIdentifiers{
-			ApplicationIds: &ttnpb.ApplicationIdentifiers{ApplicationId: fmt.Sprintf("firefly-%v", ffdev.OrganizationID)},
+			ApplicationIds: &ttnpb.ApplicationIdentifiers{ApplicationId: s.config.appID},
 			DeviceId:       "eui-" + strings.ToLower(ffdev.EUI),
 		},
-		MacSettings: &ttnpb.MACSettings{Rx2DataRateIndex: &ttnpb.DataRateIndexValue{Value: ttnpb.DataRateIndex(ffdev.Rx2DataRate)}},
+		MacSettings: &ttnpb.MACSettings{
+			Rx2DataRateIndex:       &ttnpb.DataRateIndexValue{Value: ttnpb.DataRateIndex(ffdev.Rx2DataRate)},
+			StatusCountPeriodicity: &pbtypes.UInt32Value{Value: 0},
+			StatusTimePeriodicity:  pbtypes.DurationProto(0),
+		},
 		// MacState:       &ttnpb.MACState{},
-		RootKeys:       &ttnpb.RootKeys{AppKey: &ttnpb.KeyEnvelope{}},
-		Session:        &ttnpb.Session{Keys: &ttnpb.SessionKeys{AppSKey: &ttnpb.KeyEnvelope{}, NwkSEncKey: &ttnpb.KeyEnvelope{}}},
+		RootKeys:       &ttnpb.RootKeys{},
 		SupportsClassC: ffdev.ClassC,
+		SupportsJoin:   ffdev.ApplicationKey != "",
 	}
+	v3dev.Attributes = make(map[string]string)
 	v3dev.Ids.DevEui, err = unmarshalTextToBytes(&types.EUI64{}, ffdev.EUI)
 	if err != nil {
 		return nil, err
@@ -61,22 +68,51 @@ func (s Source) ExportDevice(devEUI string) (*ttnpb.EndDevice, error) {
 	if err != nil {
 		return nil, err
 	}
-	v3dev.RootKeys.AppKey.Key, err = unmarshalTextToBytes(&types.AES128Key{}, ffdev.ApplicationKey)
-	if err != nil {
-		return nil, err
+	if v3dev.SupportsJoin {
+		v3dev.RootKeys.AppKey = &ttnpb.KeyEnvelope{}
+		v3dev.RootKeys.AppKey.Key, err = unmarshalTextToBytes(&types.AES128Key{}, ffdev.ApplicationKey)
+		if err != nil {
+			return nil, err
+		}
 	}
-	v3dev.Session.DevAddr, err = unmarshalTextToBytes(&types.DevAddr{}, ffdev.Address)
-	if err != nil {
-		return nil, err
+	hasSession := ffdev.Address != "" && ffdev.NetworkSessionKey != "" && ffdev.ApplicationSessionKey != ""
+	if hasSession || !v3dev.SupportsJoin {
+		v3dev.Session = &ttnpb.Session{Keys: &ttnpb.SessionKeys{AppSKey: &ttnpb.KeyEnvelope{}, NwkSEncKey: &ttnpb.KeyEnvelope{}}}
+		v3dev.Session.DevAddr, err = unmarshalTextToBytes(&types.DevAddr{}, ffdev.Address)
+		if err != nil {
+			return nil, err
+		}
+		v3dev.Session.Keys.AppSKey.Key, err = unmarshalTextToBytes(&types.AES128Key{}, ffdev.ApplicationSessionKey)
+		if err != nil {
+			return nil, err
+		}
+		v3dev.Session.Keys.NwkSEncKey.Key, err = unmarshalTextToBytes(&types.AES128Key{}, ffdev.NetworkSessionKey)
+		if err != nil {
+			return nil, err
+		}
+		v3dev.Session.LastAFCntDown = uint32(ffdev.FrameCounter)
+		v3dev.Session.LastNFCntDown = uint32(ffdev.FrameCounter)
+		packet, err := devices.GetLastPacket()
+		if err != nil {
+			return nil, err
+		}
+		v3dev.Session.LastFCntUp = uint32(packet.FCnt)
 	}
-	v3dev.Session.Keys.AppSKey.Key, err = unmarshalTextToBytes(&types.AES128Key{}, ffdev.ApplicationSessionKey)
-	if err != nil {
-		return nil, err
+
+	if !s.config.dryRun {
+		log.FromContext(s.ctx).WithFields(log.Fields(
+			"device_id", ffdev.Name,
+			"device_eui", ffdev.EUI,
+		)).Info("Clearing device keys")
+		r, err := api.PutDeviceUpdate(devEUI, map[string]string{
+			"address": "", "application_key": "", "application_session_key": "",
+		})
+		if err != nil {
+			return nil, err
+		}
+		r.Body.Close()
 	}
-	v3dev.Session.Keys.NwkSEncKey.Key, err = unmarshalTextToBytes(&types.AES128Key{}, ffdev.NetworkSessionKey)
-	if err != nil {
-		return nil, err
-	}
+
 	return v3dev, nil
 }
 
@@ -84,7 +120,7 @@ func (s Source) ExportDevice(devEUI string) (*ttnpb.EndDevice, error) {
 func (s Source) RangeDevices(appID string, f func(source.Source, string) error) error {
 	// req, err := http.Get(fmt.Sprintf("http://%s/api/v1/applications/%s/euis?auth=%s", s.config.apiURL, appID, s.config.apiKey))
 	var (
-		devs []*devices.Device
+		devs []devices.Device
 		err  error
 	)
 	logger.WithField("app-id", appID).Debug("App ID")
